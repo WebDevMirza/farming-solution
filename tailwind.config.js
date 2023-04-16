2f8e704cca027f16362d45d588b9adff58f67c31
WebDev Mirza <webdevmirza@gmail.com>
2023-04-16 23:04:32 +0600

    feat: add color and remove content

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,11 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      content: {
-        clock: 'url("./src/assets/svgs/clock.svg")',
-        arrowright: 'url("./src/assets/svgs/arrow-right.svg")',
-      },
       colors: {
         black222: "#222222",
         appgreen: "#008000",
         appblue: "#1E87F0",
+        appblackish: "#5F675D",
       },
       fontFamily: {
         roboto: ["Roboto", "sans-serif"],

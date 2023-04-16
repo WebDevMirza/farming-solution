4f5317ccef120c8a471ec9357f6a9765b0c04aa2
WebDev Mirza <webdevmirza@gmail.com>
2023-04-16 23:06:47 +0600

    feat: add header and hero

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,17 @@
+import Header from "../components/Header";
+import Hero from "../components/Hero";
 import Timing from "../components/Timing";
 
 const Home = () => {
   return (
     <>
       <Timing />
+      <header>
+        <Header />
+      </header>
+      <main>
+        <Hero />
+      </main>
     </>
   );
 };

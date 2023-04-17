090044518b48650f49584915e651202c44790a79
WebDev Mirza <webdevmirza@gmail.com>
2023-04-17 20:09:14 +0600

    feat: add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,13 @@ export default {
       colors: {
         black222: "#222222",
         appgreen: "#008000",
+        appdarkgreen: "#01340E",
         appblue: "#1E87F0",
         appblackish: "#5F675D",
         appgray: "#C4C4C4",
+        apporange: "#F26E21",
+        apporangehover: "#d45004",
+        appbgfooter: "#111111",
       },
       fontFamily: {
         roboto: ["Roboto", "sans-serif"],

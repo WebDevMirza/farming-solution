02c5e87324645d25b961bd0c95ea2e1078f3e6e4
WebDev Mirza <webdevmirza@gmail.com>
2023-04-18 07:42:33 +0600

    refactor: change code for router and outlet related stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import Timing from "./components/Timing";
 import Home from "./pages/Home";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <>
-      <Home />
+      <Timing />
+      <header className="lg:sticky lg:top-0 lg:bg-white lg:z-10">
+        <Header />
+      </header>
+
+      <Outlet />
+
+      <footer>
+        <Footer />
+      </footer>
     </>
   );
 };

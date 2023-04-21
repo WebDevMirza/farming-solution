891239a85df9a5adc367535c803923889894dc89
WebDev Mirza <webdevmirza@gmail.com>
2023-04-21 16:22:57 +0600

    fix: add functionality of links

--- src/components/LearnCard.tsx
+++ src/components/LearnCard.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Equipments from "../assets/equipments.webp";
 
 const LearnCard = () => {
@@ -11,12 +12,18 @@ const LearnCard = () => {
           <div className="mt-8 lg:mt-0 bg-benzene px-6 lg:px-10 flex flex-col gap-2 items-center justify-center">
             <h1 className="font-bold text-2xl">Worried About The Idling Yard?</h1>
             <p>Start hiring our equipment(s) today!</p>
-            <a
-              href="#"
+            <Link
+              onClick={() => {
+                window.scroll({
+                  top: 0,
+                  left: 0,
+                });
+              }}
+              to="/equipments"
               className="bg-apporange hover:bg-apporangehover px-4 py-2 mt-4 rounded-sm uppercase text-white tracking-wide"
             >
               Learn more
-            </a>
+            </Link>
           </div>
         </div>
       </div>

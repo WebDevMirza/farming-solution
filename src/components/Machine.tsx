6d6620ce8ac01cd542063825106ffef6220dfae9
WebDev Mirza <webdevmirza@gmail.com>
2023-04-21 16:22:36 +0600

    fix: add typescript types

--- src/components/Machine.tsx
+++ src/components/Machine.tsx
@@ -1,15 +1,29 @@
+import { Link } from "react-router-dom";
+
 type MachineCompoType = {
   img: string;
-  title: string;
+  title: EquipmentType;
 };
+
+type EquipmentType = "excavators" | "scissor lifts" | "boom lifts" | "forklifts" | "compaction" | "roller";
 
 const Machine = ({ img, title }: MachineCompoType) => {
   return (
     <>
-      <div className="bg-appgray w-fit mx-auto lg:grow rounded">
+      <Link
+        onClick={() => {
+          window.scroll({
+            top: 0,
+            left: 0,
+          });
+        }}
+        state={{ type: title }}
+        to="/equipments"
+        className="bg-appgray w-fit mx-auto lg:grow rounded"
+      >
         <img className="block p-2 w-full rounded-xl" src={img} alt="photo" />
         <p className="text-center font-bold text-xl uppercase py-4">{title}</p>
-      </div>
+      </Link>
     </>
   );
 };

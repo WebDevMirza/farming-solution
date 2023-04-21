731ea0fdd2aa342d7306b9748438d41392a633c1
WebDev Mirza <webdevmirza@gmail.com>
2023-04-21 13:10:35 +0600

    fix: add thank you message on submitting form

--- src/routers/router.tsx
+++ src/routers/router.tsx
@@ -5,6 +5,7 @@ import Equipments from "../pages/Equipments";
 import About from "../pages/About";
 import Contacts from "../pages/Contacts";
 import NotFound from "../pages/NotFound";
+import ContactForm, { contactAction } from "../components/ContactForm";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -13,7 +14,14 @@ const router = createBrowserRouter(
         <Route index element={<Home />} />
         <Route path="/equipments" element={<Equipments />} />
         <Route path="/about" element={<About />} />
-        <Route path="/contacts" element={<Contacts />} />
+        <Route path="/contacts" element={<Contacts />}>
+          <Route index element={<ContactForm />} />
+          <Route
+            path="/contacts/thankyou"
+            element={<h1 className="thanks">Thank you. We will contact soon.</h1>}
+            action={contactAction}
+          />
+        </Route>
       </Route>
       <Route path="*" element={<NotFound />} />
     </>

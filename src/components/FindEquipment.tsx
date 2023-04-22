9ec1a3ee54039658f4ff5dcaa457b050b194ee7c
WebDev Mirza <webdevmirza@gmail.com>
2023-04-22 14:15:08 +0600

    refactor: introduce form

--- src/components/FindEquipment.tsx
+++ src/components/FindEquipment.tsx
@@ -4,23 +4,32 @@ const FindEquipment = () => {
       <div className="absolute -top-[17rem] lg:-top-40 left-1/2 -translate-x-1/2 max-w-[800px] w-[90%]">
         <div className="bg-appdarkgreen text-white px-4 py-6 flex flex-col gap-6 lg:items-center rounded">
           <h1 className="text-2xl text-center uppercase">Find The Right Equipment</h1>
-          <div className="flex flex-wrap flex-col lg:flex-row gap-4 lg:gap-8 px-2">
-            <input
-              className="lg:w-[30%] px-4 py-1.5 rounded text-appbgfooter"
-              type="text"
-              name="machine"
-              id="machine"
-              placeholder="Equipment Name..."
-            />
-            <input
-              className="lg:w-[30%] px-4 py-1.5 rounded text-appbgfooter"
-              type="text"
-              name="machinetype"
-              id="machinetype"
-              placeholder="Equipment Type"
-            />
-            <button className="bg-black px-6 py-1.5 rounded">Find Equipment</button>
-          </div>
+          <form action="/equipments" method="get">
+            <div className="flex flex-wrap flex-col lg:flex-row gap-4 lg:gap-8 px-2">
+              <input
+                className="lg:w-[30%] px-4 py-1.5 rounded text-appbgfooter"
+                type="text"
+                name="machine"
+                id="machine"
+                placeholder="Equipment Name..."
+              />
+              <select
+                className="dropdown lg:w-[25%] px-3 py-1.5 rounded text-appbgfooter"
+                name="equipment"
+                id="equipment"
+              >
+                <option value="excavators">Excavators</option>
+                <option value="scissor lifts">Scissor Lifts</option>
+                <option value="boom lifts">Boom Lifts</option>
+                <option value="forklifts">Forklifts</option>
+                <option value="compaction">Compaction</option>
+                <option value="roller">Roller</option>
+              </select>
+              <button type="submit" className="bg-black px-6 py-1.5 rounded">
+                Find Equipment
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,67 +6,69 @@ const Hero = () => {
           <h1 className="text-3xl uppercase text-center leading-8 mb-52">
             Your One-Stop Source For All Equipment Rental Needs
           </h1>
-          <div className="relative rounded-lg overflow-hidden mt-8 w-[95%] mx-auto">
-            <input
-              className="pl-4 pr-[6rem] py-4 lg:min-w-[550px] text-appblackish"
-              type="text"
-              name="search"
-              id="search"
-              placeholder="Search for something..."
-            />
-            <button className="bg-appblackish absolute inset-y-0 right-0 px-3 lg:px-6 hover:opacity-95">
-              <svg
-                className="inline"
-                width="30"
-                height="25"
-                viewBox="4 0 35 37"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <g filter="url(#filter0_d_25_88)">
-                  <path
-                    d="M15.728 25.3333C21.6441 25.3333 26.4401 20.5577 26.4401 14.6667C26.4401 8.77563 21.6441 4 15.728 4C9.81186 4 5.01588 8.77563 5.01588 14.6667C5.01588 20.5577 9.81186 25.3333 15.728 25.3333Z"
-                    stroke="#fff"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                  <path
-                    d="M29.1181 28L23.2934 22.2"
-                    stroke="#fff"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                </g>
-                <defs>
-                  <filter
-                    id="filter0_d_25_88"
-                    x="-3.00116"
-                    y="0"
-                    width="40.1363"
-                    height="40"
-                    filterUnits="userSpaceOnUse"
-                    colorInterpolationFilters="sRGB"
-                  >
-                    <feFlood floodOpacity="0" result="BackgroundImageFix" />
-                    <feColorMatrix
-                      in="SourceAlpha"
-                      type="matrix"
-                      values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-                      result="hardAlpha"
+          <form action="/equipments" method="get">
+            <div className="relative rounded-lg overflow-hidden mt-8 w-[95%] mx-auto">
+              <input
+                className="pl-4 pr-[6rem] py-4 lg:min-w-[550px] text-appblackish"
+                type="text"
+                name="search"
+                id="search"
+                placeholder="Search for something..."
+              />
+              <button type="submit" className="bg-appblackish absolute inset-y-0 right-0 px-3 lg:px-6 hover:opacity-95">
+                <svg
+                  className="inline"
+                  width="30"
+                  height="25"
+                  viewBox="4 0 35 37"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g filter="url(#filter0_d_25_88)">
+                    <path
+                      d="M15.728 25.3333C21.6441 25.3333 26.4401 20.5577 26.4401 14.6667C26.4401 8.77563 21.6441 4 15.728 4C9.81186 4 5.01588 8.77563 5.01588 14.6667C5.01588 20.5577 9.81186 25.3333 15.728 25.3333Z"
+                      stroke="#fff"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                     />
-                    <feOffset dy="4" />
-                    <feGaussianBlur stdDeviation="2" />
-                    <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
-                    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_25_88" />
-                    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_25_88" result="shape" />
-                  </filter>
-                </defs>
-              </svg>
-              Search
-            </button>
-          </div>
+                    <path
+                      d="M29.1181 28L23.2934 22.2"
+                      stroke="#fff"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </g>
+                  <defs>
+                    <filter
+                      id="filter0_d_25_88"
+                      x="-3.00116"
+                      y="0"
+                      width="40.1363"
+                      height="40"
+                      filterUnits="userSpaceOnUse"
+                      colorInterpolationFilters="sRGB"
+                    >
+                      <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                      <feColorMatrix
+                        in="SourceAlpha"
+                        type="matrix"
+                        values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                        result="hardAlpha"
+                      />
+                      <feOffset dy="4" />
+                      <feGaussianBlur stdDeviation="2" />
+                      <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+                      <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_25_88" />
+                      <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_25_88" result="shape" />
+                    </filter>
+                  </defs>
+                </svg>
+                Search
+              </button>
+            </div>
+          </form>
           <p className="text-center">Top-Of-The-Line Equipment Ready At Flexible Rates Around</p>
         </div>
       </div>
